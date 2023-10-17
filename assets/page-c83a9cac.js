ef0e5e55877ffe7a710013292a0595e1e1c24102
Aritan Huang <aritan840703@gmail.com>
2023-10-17 15:58:14 +0800

    Updates

---
+++ assets/page-c83a9cac.js
@@ -0,0 +1,1 @@
+import{a as o}from"./axios-fd1fa517.js";const n=document.querySelector("#title"),e=document.querySelector("#content"),c="https://json-auth-test.onrender.com",s=location.href.split("=")[1];o.get(`${c}/todos/${s}`).then(function(t){n.textContent=t.data.id,e.textContent=t.data.content}).catch(function(t){console.log(t)});
